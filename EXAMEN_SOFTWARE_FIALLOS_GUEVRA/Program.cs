d47a00b8a83f4b33e96b242f206b187177fcf5c4
JOSU <jfiallos7065@uta.edu.ec>
2025-10-07 10:30:39 -0500

    FIALLOS-GUEVARA 10:30 INICIO DE LA PRUEBA

---
+++ EXAMEN_SOFTWARE_FIALLOS_GUEVRA/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EXAMEN_SOFTWARE_FIALLOS_GUEVRA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
